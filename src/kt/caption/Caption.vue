f2370a0fc67612313833f1cabcc1bc0154c07d85
almaz <almaz_73@mail.ru>
2023-05-10 21:50:13 +0300

    edit

--- src/kt/caption/Caption.vue
+++ src/kt/caption/Caption.vue
@@ -1,6 +1,36 @@
 <script setup>
 import StatusButtons from "./StatusButtons.vue";
 import StopWatch from "./StopWatch.vue";
+
+import {ref} from 'vue'
+
+const value = ref('')
+
+const options = [
+  {
+    value: 'Option1',
+    label: 'События',
+  },
+  {
+    value: 'Option2',
+    label: 'Отметки',
+  },
+  {
+    value: 'Option3',
+    label: 'Идентифицировать пациента',
+  },
+  {
+    value: 'Option4',
+    label: 'Поиск пациента по реестру',
+  },
+  {
+    value: 'Option5',
+    label: 'Телефонная книга',
+  },
+  {
+    value: 'Option6',
+    label: 'Поиск аудиозаписей',
+  }]
 </script>
 
 <template>
@@ -11,8 +41,13 @@ import StopWatch from "./StopWatch.vue";
       <el-button class="special-little">
         Изменить признак вызова
       </el-button>
-      <el-select>
-        Действия
+      <el-select v-model="value" class="m-2" placeholder="Действия">
+        <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+        />
       </el-select>
       <StopWatch/>
     </div>
